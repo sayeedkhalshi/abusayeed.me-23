3d9331660e873e5207e2fe5c2a77100f9e4ac22a
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2023-10-22 02:00:00 +0600

    add build folder as server can not build

---
+++ frontend/build/static/chunks/app/page-4d276c55f8c2229e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{865:function(n,e,u){Promise.resolve().then(u.t.bind(u,3359,23))}},function(n){n.O(0,[359,195,338,744],function(){return n(n.s=865)}),_N_E=n.O()}]);
